db0a659dbb337109b1ceb59108ce2c85b80c181d
Alexander Malinov <alieksandr.malinov@mail.ru>
2020-07-27 17:54:26 +0300

    add webpack and change file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import axios from 'axios';
-import onChange from 'onchange';
+const axios = require('axios');
+const onChange = require('on-change');
 
 const routes = {
   smallData: () => 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
   bigData: () => 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
 };
+
+const app = () => {
+  const userChoose = confirm('Do you want see all data? If no - press "cancel"');
+  console.log(userChoose);
+  const state = {
+    dataSet: null
+  };
+
+
+
+
+
+};
+
+app();
